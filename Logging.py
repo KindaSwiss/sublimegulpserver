8bf248f304e7188e279a37ff06c8fc41f54e1df8
kindaswiss <kindaswiss@gmail.com>
2015-05-20 01:01:23 -0700

    Add console log

---
+++ Logging.py
@@ -0,0 +1,32 @@
+
+from GulpServer.Settings import Settings
+
+
+
+user_settings = None
+
+
+
+def plugin_loaded():
+	global user_settings
+	user_settings = Settings()
+
+
+
+
+
+class Console(object):
+
+	def log(self, *args):
+
+		if user_settings.get('dev'):
+			print(*args)
+
+
+
+
+
+
+
+
+
