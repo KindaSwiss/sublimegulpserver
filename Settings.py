a12de6f0459778c25ea3a16aa13462118ab15b0e
anthonykoch <kindaswiss@gmail.com>
2016-01-18 13:56:55 -0700

    Remove default settings
    Apparently Sublime Text loads in default settings and then loads
    in user settings into a Settings object.

--- Settings.py
+++ Settings.py
@@ -5,36 +5,19 @@ class Settings(object):
 	""" A wrapper for a sublime.Settings object """
 	# The sublime.Settings object
 	loaded_settings = None
+	path = 'EditorConnect.sublime-settings'
 
-	# Default settings will be used where no user settings have been defined
-	default_settings = {
-		"scroll_to_error": True,
-		"show_icon_at_error": True,
-		"show_error_popup": True,
-		"show_error_status": True,
-		"show_status_in_view": False,
-		"error_status_format": "{plugin_name} error, Line {line}, File: {file_name}",
-		"error_popup_format": "Line {line}; {message}",
-		"error_icon": "bookmark",
-		"port": 35048,
-	}
-	settings_path = 'EditorConnect.sublime-settings'
-
-	def __init__(self, settings_path=None, load=True):
-		if isinstance(settings_path, str):
-			# Case sensitive
-			self.settings_path = settings_path
-
+	def __init__(self, load=True):
 		if load:
 			self.load()
 
 	def load(self):
-		self.loaded_settings = sublime.load_settings(self.settings_path)
-		self.loaded_settings.clear_on_change(self.settings_path)
-		self.loaded_settings.add_on_change(self.settings_path, self.load)
+		self.loaded_settings = sublime.load_settings(self.path)
+		self.loaded_settings.clear_on_change(self.path)
+		self.loaded_settings.add_on_change(self.path, self.load)
 
 	def save(self):
-		sublime.save_settings(self.settings_path)
+		sublime.save_settings(self.path)
 
 	def set(self, key, value):
 		""" Set a value into the sublime.Settings object """
@@ -42,4 +25,4 @@ class Settings(object):
 
 	def get(self, key, default=None):
 		""" Get a value by key from the settings. Loads from default settings if key doesn't the exist. """
-		return self.loaded_settings.get(key, self.default_settings[key])
+		return self.loaded_settings.get(key)
